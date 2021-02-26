3e789d190658e6f646652675f682602faf4a02d8
Giancarlo Zuliani <slaigox@gmail.com>
2021-02-26 23:03:35 +0100

    minor fix

--- main/resources/views/pages/index.blade.php
+++ main/resources/views/pages/index.blade.php
@@ -4,8 +4,8 @@
     <div class="typologybox" v-for="type in randomTypoArray" @click="getRestaurant(type.id)" v-if="showtypo">
         @{{type.typology}}
     </div>
-    <a v-for="rest in restaurantArray" href="{{route('show-menu',@rest.id)}}">
-        <div  class="typologybox" v-if="showRestaurant" v-for="rest in restaurantArray">
+    <a v-for="rest in restaurantArray" :href=`{{route('show-menu','')}}/${rest.id}`>
+        <div  class="typologybox" v-if="showRestaurant">
             @{{rest.name}}
         </div>
     </a>
