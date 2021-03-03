977eb121e2b1f5b5db5daad21b0c5a769cf6b8f2
Giancarlo Zuliani <slaigox@gmail.com>
2021-03-03 16:17:17 +0100

    header minor fix

--- main/resources/views/components/header.blade.php
+++ main/resources/views/components/header.blade.php
@@ -1,7 +1,7 @@
-<header>
+{{-- <header>
     <nav>
         <a href="{{route('index')}}">
             <img src="{{asset('images/logo.svg')}}" alt="Fooduro logo">
         </a>
     </nav>
-</header>
+</header> --}}
