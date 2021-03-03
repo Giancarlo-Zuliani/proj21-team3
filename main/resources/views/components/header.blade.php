11b55905cc07aec0c1b0299cb8691b62e03e5427
nataliavrs <verasnatali4@gmail.com>
2021-03-03 13:11:52 +0100

    Testing svg images header

--- main/resources/views/components/header.blade.php
+++ main/resources/views/components/header.blade.php
@@ -1,4 +1,7 @@
 <header>
-    {{-- <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1920 499"><path d="M0,481V0H1920V499S1026,97,0,481Z" style="fill:#fcc244"/></svg> --}}
-    HEADER
+    <nav>
+        <a href="{{route('index')}}">
+            <img src="{{asset('storage/img/logo.svg')}}" alt="Fooduro logo">
+        </a>
+    </nav>
 </header>
--- main/resources/views/layouts/app.blade.php
+++ main/resources/views/layouts/app.blade.php
@@ -24,11 +24,17 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <nav class="navbar nav-dash navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
+                {{-- <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
+                    Fooduro
+                </a> --}}
+                <a class="navbar-brand" href="{{route('index')}}">
+                    {{-- <img src="{{asset('storage/img/logo.svg')}}" alt="Fooduro logo"> --}}
+                    Fooduro
                 </a>
+
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
