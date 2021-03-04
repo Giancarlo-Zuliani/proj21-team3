63b5496c0c39bde08a6991a43471affa600d281c
nataliavrs <verasnatali4@gmail.com>
2021-03-04 12:49:33 +0100

    Header image

--- main/resources/views/components/header.blade.php
+++ main/resources/views/components/header.blade.php
@@ -1,7 +1,7 @@
-{{-- <header>
+<header>
     <nav>
         <a href="{{route('index')}}">
-            <img src="{{asset('images/logo.svg')}}" alt="Fooduro logo">
+            <img src="{{asset('storage/assets/header.svg')}}" alt="header">
         </a>
     </nav>
-</header> --}}
+</header>
