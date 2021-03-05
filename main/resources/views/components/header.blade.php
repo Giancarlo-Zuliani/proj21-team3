85209dc238b9959cc7f740fce7251c67c1726c5a
Natália <verasnatali4@gmail.com>
2021-03-05 21:43:28 +0100

    Header e COPIA BRAINTREE BOOLBNB

--- main/resources/views/components/header.blade.php
+++ main/resources/views/components/header.blade.php
@@ -1,7 +1,22 @@
 <header>
-    {{-- <nav>
+    {{-- <nav> --}}
+        <div class="logo">
+            <a href="{{route('index')}}">
+                <img src="{{asset('storage/assets/logo.svg')}}" alt="logo">
+            </a>
+        </div>
+        
+        <div class="btns">
+            <button class="btn btn-primary">Log in</button>
+            <button class="btn btn-primary">Register</button>
+            <i class="far fa-user-circle"></i>
+        </div>
+    {{-- </nav> --}}
+
+    {{-- <div>
         <a href="{{route('index')}}">
             <img src="{{asset('storage/assets/header.svg')}}" alt="header">
         </a>
-    </nav> --}}
+    </div> --}}
+   
 </header>
--- main/resources/views/pages/checkout.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('layouts.main-layout')
-
-@section('content')
-    <a href="{{route('index')}}">home</a>
-    <h1>checkout con brain tree</h1>
-@endsection
