ee96eeb6db5899ebb76f251e652374d6f08195e5
nataliavrs <verasnatali4@gmail.com>
2021-03-05 22:32:27 +0100

    Index header & footer UI test

--- main/resources/views/components/footer.blade.php
+++ main/resources/views/components/footer.blade.php
@@ -1,9 +1,7 @@
 <footer>
-       {{--  <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1920 499"><defs><style>.cls-1{fill:#2e294d;}</style></defs>
-            <path class="cls-1" d="M1917,440V921H-3V422S891,824,1917,440Z" transform="translate(3 -422)"/>
-        </svg> --}}
-
-        {{-- <object type="image/svg+xml" data="bblogo.svg">
-            Your browser does not support SVGs
-        </object> --}}
+        <div class="logo">
+            <a href="{{route('index')}}">
+                <img src="{{asset('storage/assets/footer.svg')}}" alt="footer">
+            </a>
+        </div>
 </footer>
