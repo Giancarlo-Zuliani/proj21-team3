67c1ec5a5e5b60ddbd524797b52bceb2a9180cb4
Giancarlo Zuliani <slaigox@gmail.com>
2021-03-08 22:43:08 +0100

    user chart added

--- main/public/js/statistics.js
+++ main/public/js/statistics.js
@@ -1,59 +1,95 @@
 const statistics = new Vue({
     el: '#angelo',
     data: {
-        dataset: '',
+        dataset: [],
         canvas: '',
     },
-    mounted: function () {
-            this.initChart();
-            this.getStatistics();
+    mounted: function() {
+        this.initChart();
+        this.getStatistics();
     },
     methods: {
-        initChart () {
+        initChart() {
             var ctx = document.getElementById('myChart').getContext('2d');
             this.canvas = new Chart(ctx, {
-            type: 'bar',
-            data: {
-                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-                datasets: [{
-                    label: 'Ordini',
-                    data: [10, 10, 3, 5, 2, 3],
-                    backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)'
-                    ],
-                    borderColor: [
-                        'rgba(255, 99, 132, 1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(75, 192, 192, 1)',
-                        'rgba(153, 102, 255, 1)',
-                        'rgba(255, 159, 64, 1)'
-                    ],
-                    borderWidth: 1
-                }]
-            },
-            options: {
-                scales: {
-                    yAxes: [{
-                        ticks: {
-                            beginAtZero: true
-                        }
+                type: 'bar',
+                data: {
+                    labels: ['Gennaio', 'Febbraio', 'Marzo', 'Aprile', 'Maggio', 'Giugno', 'Luglio', 'Agosto', 'Settembre', 'Ottobre', 'Novembre', 'Dicembre'],
+                    datasets: [{
+                        label: 'Ordini',
+                        data: this.dataset,
+                        backgroundColor: [
+                            'rgba(255, 99, 132, 0.2)',
+                            'rgba(54, 162, 235, 0.2)',
+                            'rgba(255, 206, 86, 0.2)',
+                            'rgba(75, 192, 192, 0.2)',
+                            'rgba(153, 102, 255, 0.2)',
+                            'rgba(255, 159, 64, 0.2)',
+                            'rgba(255, 99, 132, 0.2)',
+                            'rgba(54, 162, 235, 0.2)',
+                            'rgba(255, 206, 86, 0.2)',
+                            'rgba(75, 192, 192, 0.2)',
+                            'rgba(153, 102, 255, 0.2)',
+                            'rgba(255, 159, 64, 0.2)'
+                        ],
+                        borderColor: [
+                            'rgba(255, 99, 132, 1)',
+                            'rgba(54, 162, 235, 1)',
+                            'rgba(255, 206, 86, 1)',
+                            'rgba(75, 192, 192, 1)',
+                            'rgba(153, 102, 255, 1)',
+                            'rgba(255, 159, 64, 1)',
+                            'rgba(255, 99, 132, 1)',
+                            'rgba(54, 162, 235, 1)',
+                            'rgba(255, 206, 86, 1)',
+                            'rgba(75, 192, 192, 1)',
+                            'rgba(153, 102, 255, 1)',
+                            'rgba(255, 159, 64, 1)'
+                        ],
+                        borderWidth: 1
                     }]
+                },
+                options: {
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                beginAtZero: true
+                            }
+                        }]
+                    }
                 }
-            }
             });
         },
-        getStatistics () {
-          let id = document.getElementById('vendorId').value;
-          let url = 'http://127.0.0.1:8000/get-time/' + id;
-          axios.get(url).then(response => {
-            console.log(response.data);
-          });
+        getStatistics() {
+            let id = document.getElementById('vendorId').value;
+            let url = 'http://127.0.0.1:8000/get-time/' + id;
+            axios.get(url).then(response => {
+                this.dataset = [];
+                let orderCreatedAt = [];
+                console.log(response.data);
+                response.data.forEach(element => {
+                    orderCreatedAt.push(element.created_at.slice(0, 7));
+                });
+                let year = '2021-'
+                for (let y = 1; y <= 12; y++) {
+                    let count = 0;
+                    for (let i = 0; i < orderCreatedAt.length; i++) {
+                        if (y >= 10) {
+                            m = y;
+                            m.toString();
+                        } else {
+                            m = "0" + y.toString();
+
+                        }
+                        console.log(m);
+                        if (orderCreatedAt[i] == year + m) {
+                            count++
+                        }
+                    }
+                    this.dataset.push(count);
+                }
+                this.initChart(this.dataset);
+            });
         },
     },
 });
