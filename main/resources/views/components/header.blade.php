c5aac55628be32ce86ffa6294dfd459573338a35
nataliavrs <verasnatali4@gmail.com>
2021-03-11 10:48:09 +0100

    Nav bar dashboard link

--- main/resources/views/components/header.blade.php
+++ main/resources/views/components/header.blade.php
@@ -59,14 +59,20 @@
                             </a>
                             
                             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                
+
+                                <a class="dropdown-item" href="{{ route('user-show')}}">                                     
+                                    Il tuo profilo
+                                </a>
+
+                                <a class="dropdown-item" href="{{ route('home')}}">                                     
+                                    Dashboard
+                                </a>
+
                                 <a class="dropdown-item" href="{{ route('logout') }}"
                                 onclick="event.preventDefault();
                                                 document.getElementById('logout-form').submit();">
                                     {{ __('Logout') }}
-                                </a>
-
-                                <a class="dropdown-item" href="{{ route('user-show')}}">                                     
-                                    Il tuo profilo
                                 </a>
 
                                 <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
