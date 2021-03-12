a0dacc7d6c2d34d3944e2cfd8710b1333bbf9be6
Giancarlo Zuliani <slaigox@gmail.com>
2021-03-12 15:52:30 +0100

    avatar responsiveness fixed

--- main/resources/views/components/footer.blade.php
+++ main/resources/views/components/footer.blade.php
@@ -3,12 +3,16 @@
 <footer class="bg-dark text-center text-white">
 {{--     <img src="{{asset('storage/assets/footer.svg')}}" alt="footer">
  --}}    <!-- Grid container -->
-    <div class="container-fluid p-4">  
-        <div class="row ">
+    <div class="container p-4">  
+        <div class="row text-center">
           <div class="col-lg-2 col-md-11 mb-4 mb-md-0">
             <img id="footersvg" src="{{asset('storage/assets/logo-yellow.svg')}}" alt="">          
           </div>
-          <div class="col-lg-2 col-md-6 mb-4 mb-md-0">
+        </div>
+        
+        <div class="row  p-4">
+          {{-- FIRST AVATAR --}}
+          <div class="col-xl-2 offset-xl-1 col-lg-3 col-md-6 col-sm-12 mb-4 p-6 mb-md-0 footer_avatar">
             <ul class="list-unstyled mb-0">
               <li>
                 <div class="flip-card">
@@ -17,16 +21,16 @@
                       <img src="{{asset('storage/assets/av-gz.svg')}}" alt="Avatar">
                     </div>
                     <div class="flip-card-back">
-                      <h1>John Doe</h1> 
-                      <p>Architect & Engineer</p> 
-                      <p>We love that guy</p>
+                      <h5>Dario Alessio</h5>
+                      <a class="github-button footer_git_link" href="https://github.com/Giancarlo-Zuliani" data-color-scheme="no-preference: dark; light: dark; dark: dark;" data-size="large" aria-label="Follow @Giancarlo-Zuliani on GitHub">Follow @Giancarlo-Zuliani</a>
                     </div>
                   </div>
                 </div>
               </li>
             </ul>
           </div>
-          <div class="col-lg-2 col-md-6 mb-4 mb-md-0">
+          <div class="col-xl-2 offset-xl-1 col-lg-3 col-md-6 col-sm-12 mb-4 p-6 mb-md-0 footer_avatar">
+          
             <ul class="list-unstyled mb-0">
               <li>
                 <div class="flip-card">
@@ -35,16 +39,16 @@
                       <img src="{{asset('storage/assets/av-gz.svg')}}" alt="Avatar">
                     </div>
                     <div class="flip-card-back">
-                      <h1>John Doe</h1> 
-                      <p>Architect & Engineer</p> 
-                      <p>We love that guy</p>
+                      <h5>Dario Alessio</h5>
+                      <a class="github-button" href="https://github.com/Giancarlo-Zuliani" data-color-scheme="no-preference: dark; light: dark; dark: dark;" aria-label="Follow @Giancarlo-Zuliani on GitHub">Follow @Giancarlo-Zuliani</a>
                     </div>
                   </div>
                 </div>
               </li>
             </ul>
           </div>
-          <div class="col-lg-2 col-md-6 mb-4 mb-md-0">
+          <div class="col-xl-2 offset-xl-1 col-lg-3 col-md-6 col-sm-12 mb-4 p-6 mb-md-0 footer_avatar">
+          
             <ul class="list-unstyled mb-0">
               <li>
                 <div class="flip-card">
@@ -53,16 +57,16 @@
                       <img src="{{asset('storage/assets/av-gz.svg')}}" alt="Avatar">
                     </div>
                     <div class="flip-card-back">
-                      <h1>John Doe</h1> 
-                      <p>Architect & Engineer</p> 
-                      <p>We love that guy</p>
+                      <h5>Dario Alessio</h5>
+                      <a class="github-button" href="https://github.com/Giancarlo-Zuliani" data-color-scheme="no-preference: dark; light: dark; dark: dark;" data-size="large" aria-label="Follow @Giancarlo-Zuliani on GitHub">Follow @Giancarlo-Zuliani</a>
                     </div>
                   </div>
                 </div>
               </li>
             </ul>
           </div>
-          <div class="col-lg-2 col-md-6 mb-4 mb-md-0">
+          <div class="col-xl-2 offset-xl-1 col-lg-3 col-md-6 col-sm-12 mb-4 p-6 mb-md-0 footer_avatar">
+         
             <ul class="list-unstyled mb-0">
               <li>
                 <div class="flip-card">
@@ -71,16 +75,16 @@
                       <img src="{{asset('storage/assets/av-gz.svg')}}" alt="Avatar">
                     </div>
                     <div class="flip-card-back">
-                      <h1>John Doe</h1> 
-                      <p>Architect & Engineer</p> 
-                      <p>We love that guy</p>
+                      <h5>Dario Alessio</h5>
+                      <a class="github-button" href="https://github.com/Giancarlo-Zuliani" data-color-scheme="no-preference: dark; light: dark; dark: dark;" data-size="large" aria-label="Follow @Giancarlo-Zuliani on GitHub">Follow @Giancarlo-Zuliani</a>
                     </div>
                   </div>
                 </div>
               </li>
             </ul>
           </div>
-          <div class="col-lg-2 col-md-6 mb-4 mb-md-0">
+          <div class="col-xl-2 offset-xl-1 col-lg-3 col-md-6 col-sm-12 mb-4 p-6 mb-md-0 footer_avatar">
+          
             <ul class="list-unstyled mb-0">
               <li>
                 <div class="flip-card">
@@ -89,20 +93,20 @@
                       <img src="{{asset('storage/assets/av-gz.svg')}}" alt="Avatar">
                     </div>
                     <div class="flip-card-back">
-                      <h3>Dario Alessio</h3> 
-                      <p>Architect & Engineer</p> 
-                      <p>We love that guy</p>
+                      <h5>Dario Alessio</h5>
+                      <a class="github-button" href="https://github.com/Giancarlo-Zuliani" data-color-scheme="no-preference: dark; light: dark; dark: dark;" data-size="large" aria-label="Follow @Giancarlo-Zuliani on GitHub">Follow @Giancarlo-Zuliani</a>
                     </div>
                   </div>
                 </div>
               </li>
             </ul>
           </div>
+          
         </div>
     </div>
   
     <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
       © 2020 Copyright:
-      <a class="text-white" href="https://mdbootstrap.com/">MDBootstrap.com</a>
     </div>
+    <script async defer src="https://buttons.github.io/buttons.js"></script>
   </footer>
