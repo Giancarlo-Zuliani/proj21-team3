ee37b40acb4c2892d500fd4505ed18d15756604b
AngeloCinaWD <angelo.cina@yahoo.it>
2021-03-12 11:05:13 +0100

    Assettts

--- main/resources/views/layouts/main-layout.blade.php
+++ main/resources/views/layouts/main-layout.blade.php
@@ -8,7 +8,7 @@
     <title>FooDuro</title>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
   {{-- FAVICON --}}
-  <link rel="icon" type="image/png" href="/images/favicon.png"/>
+  <link rel="icon" type="image/png" href="{{asset('storage/assets/favicon.png')}}"/>
 
   @stack('scriptPayment')
   @stack('scriptStatistics')
@@ -40,7 +40,7 @@
 
     </main>
 
-    
+
 
     {{-- <div id="prova">
       @yield('content')
