ce181eaa3c64105f82ba879c28087d3f367ca2d1
Giancarlo Zuliani <slaigox@gmail.com>
2021-03-12 18:09:15 +0100

    mail layout added

--- main/resources/views/mail/mail.blade.php
+++ main/resources/views/mail/mail.blade.php
@@ -7,12 +7,14 @@
     <title>E-mail</title>
 </head>
 <body>
+    <img src="{{asset('storage/assets/logo.svg alt="">
     
     <h1>Pagamento avvenuto con successo</h1>
     {{-- dati dell'ordine --}}
-    <p>Gentile {{$order['buyer_name']}} {{$order['buyer_lastname']}} </p>    
-    <p>Il tuo ordine del totale di {{$order['final_price']/100}}€ sta arrivando a questo indirizzo {{$order['address']}}.</p>
-    <h3>Buon appetito dallo staff di Fooduro, speriamo di vederci nel prossimo fooduro.</h3>
-        
+    <div id="mail_body">
+        <p>Gentile {{$order['buyer_name']}} {{$order['buyer_lastname']}} </p>    
+        <p>Il tuo ordine del totale di {{$order['final_price']/100}}€ sta arrivando a questo indirizzo {{$order['address']}}.</p>
+        <h3>Buon appetito dallo staff di Fooduro, speriamo di vederci nel prossimo fooduro.</h3>
+    <div>
 </body>
 </html>
