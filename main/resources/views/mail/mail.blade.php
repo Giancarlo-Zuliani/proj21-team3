928f327a4a2c7e26b5922fdbf1c0f5a26c62c37f
AngeloCinaWD <angelo.cina@yahoo.it>
2021-03-12 22:54:16 +0100

    PUsh e te ne puoi andare

--- main/resources/views/mail/mail.blade.php
+++ main/resources/views/mail/mail.blade.php
@@ -7,12 +7,12 @@
     <title>E-mail</title>
 </head>
 <body>
-    <img src="{{asset('storage/assets/logo.svg alt="">
-    
+    <img src="{{asset('storage/assets/logo.svg')}}" alt="">
+
     <h1>Pagamento avvenuto con successo</h1>
     {{-- dati dell'ordine --}}
     <div id="mail_body">
-        <p>Gentile {{$order['buyer_name']}} {{$order['buyer_lastname']}} </p>    
+        <p>Gentile {{$order['buyer_name']}} {{$order['buyer_lastname']}} </p>
         <p>Il tuo ordine del totale di {{$order['final_price']/100}}€ sta arrivando a questo indirizzo {{$order['address']}}.</p>
         <h3>Buon appetito dallo staff di Fooduro, speriamo di vederci nel prossimo fooduro.</h3>
     <div>
