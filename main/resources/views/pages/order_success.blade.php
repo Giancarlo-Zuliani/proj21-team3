0c40454961f5d713f100725d3cef10141cb7334a
Andrea Sansica <sansica.andrea99@gmail.com>
2021-03-13 11:16:16 +0100

    order success and 404

--- main/resources/views/pages/order_success.blade.php
+++ main/resources/views/pages/order_success.blade.php
@@ -1,7 +1,21 @@
 @extends('layouts.main-layout')
 
 @section('content')
-    <h1>Pagamento avvenuto con successo, informazioni nell'e-mail xD</h1>    
+  <div class="container">
+    <div class="row text-center">
+      <div class="col-lg-12">
+        <h1>Il tuo pagamento è andato a buon fine.</h1>
+      </div>
+    </div>
+    <div class="d-flex justify-content-center">
+      <img height="300px" width="300px" src="{{asset('storage/assets/pagamento.svg')}}" >
+    </div>
+    <div class="row text-center">
+      <div class="col-lg-12" style="margin-bottom: 100px;">
+        <h2>Grazie del tuo ordine! Ti abbiamo inviato un'e-mail con le tue informazioni</h2>
+      </div>
+    </div>
+  </div>
         <a id="backhome" href="{{route('index')}}" ></a>
 @endsection
 
