70b4ac50363dafcb3ae1778caada4473dd71dded
Giancarlo Zuliani <slaigox@gmail.com>
2021-03-13 15:24:18 +0100

    github link on footer fixed

--- main/resources/views/components/footer.blade.php
+++ main/resources/views/components/footer.blade.php
@@ -1,15 +1,10 @@
-
-<!-- Footer -->
 <footer class="text-center text-white">
-{{--     <img src="{{asset('storage/assets/footer.svg')}}" alt="footer">
- --}}    <!-- Grid container -->
     <div class="container-fluid p-4">  
         <div class="row align-items-center">
-          <div class="col-lg-2 col-md-2 col-sm-3 mb-4 mb-md-0">
+          <div class="col-sm-3 ">
             <img id="footersvg" src="{{asset('storage/assets/logo-yellow.svg')}}" alt="">          
           </div>
         </div>
-        
         <div class="row justify-content-center  p-4">
           {{-- FIRST AVATAR --}}
           <div class="col-xl-2 col-lg-4 col-md-6 col-sm-6 my-4">
@@ -22,7 +17,7 @@
                     </div>
                     <div class="flip-card-back">
                       <h5>Dario Alessio</h5>
-                      <a class="github-button footer_git_link" href="https://github.com/Giancarlo-Zuliani" data-color-scheme="no-preference: dark; light: dark; dark: dark;" data-size="large" aria-label="Follow @Giancarlo-Zuliani on GitHub">Follow @Giancarlo-Zuliani</a>
+                      <a class="github-button footer_git_link" href="https://github.com/DarioAlessio" data-color-scheme="no-preference: dark; light: dark; dark: dark;" data-size="large" aria-label="Follow @Giancarlo-Zuliani on GitHub">Follow @DarioAlessio</a>
                     </div>
                   </div>
                 </div>
@@ -39,8 +34,8 @@
                       <img src="{{asset('storage/assets/av-gz.svg')}}" alt="Avatar">
                     </div>
                     <div class="flip-card-back">
-                      <h5>Dario Alessio</h5>
-                      <a class="github-button" href="https://github.com/Giancarlo-Zuliani" data-color-scheme="no-preference: dark; light: dark; dark: dark;" data-size="large" aria-label="Follow @Giancarlo-Zuliani on GitHub">Follow @Giancarlo-Zuliani</a>
+                      <h5>Angelo Cina</h5>
+                      <a class="github-button" href="https://github.com/AngeloCinaWD" data-color-scheme="no-preference: dark; light: dark; dark: dark;" data-size="large" aria-label="Follow @Giancarlo-Zuliani on GitHub">Follow @AngeloCinaWD</a>
                     </div>
                   </div>
                 </div>
@@ -57,8 +52,8 @@
                       <img src="{{asset('storage/assets/av-gz.svg')}}" alt="Avatar">
                     </div>
                     <div class="flip-card-back">
-                      <h5>Dario Alessio</h5>
-                      <a class="github-button footer_git_link" href="https://github.com/Giancarlo-Zuliani" data-color-scheme="no-preference: dark; light: dark; dark: dark;" data-size="large" aria-label="Follow @Giancarlo-Zuliani on GitHub">Follow @Giancarlo-Zuliani</a>
+                      <h5>Andrea Sansica</h5>
+                      <a class="github-button footer_git_link" href="https://github.com/andreasansica" data-color-scheme="no-preference: dark; light: dark; dark: dark;" data-size="large" aria-label="Follow @Giancarlo-Zuliani on GitHub">Follow @andreasansica</a>
                     </div>
                   </div>
                 </div>
@@ -75,8 +70,8 @@
                       <img src="{{asset('storage/assets/av-gz.svg')}}" alt="Avatar">
                     </div>
                     <div class="flip-card-back">
-                      <h5>Dario Alessio</h5>
-                      <a class="github-button" href="https://github.com/Giancarlo-Zuliani" data-color-scheme="no-preference: dark; light: dark; dark: dark;" data-size="large" aria-label="Follow @Giancarlo-Zuliani on GitHub">Follow @Giancarlo-Zuliani</a>
+                      <h5>Natalia Veras</h5>
+                      <a class="github-button" href="https://github.com/nataliavrs" data-color-scheme="no-preference: dark; light: dark; dark: dark;" data-size="large" aria-label="Follow @Giancarlo-Zuliani on GitHub">Follow @nataliavrs</a>
                     </div>
                   </div>
                 </div>
@@ -93,7 +88,7 @@
                       <img src="{{asset('storage/assets/av-gz.svg')}}" alt="Avatar">
                     </div>
                     <div class="flip-card-back">
-                      <h5>Dario Alessio</h5>
+                      <h5>Giacarlo Zuliani</h5>
                       <a class="github-button footer_git_link" href="https://github.com/Giancarlo-Zuliani" data-color-scheme="no-preference: dark; light: dark; dark: dark;" data-size="large" aria-label="Follow @Giancarlo-Zuliani on GitHub">Follow @Giancarlo-Zuliani</a>
                     </div>
                   </div>
@@ -106,7 +101,7 @@
     </div>
   
     <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-      © 2020 Copyright:
+      © 2021 Copyright: Made With &#10084;&#65039; By Team3-Classe21
     </div>
     <script async defer src="https://buttons.github.io/buttons.js"></script>
   </footer>
