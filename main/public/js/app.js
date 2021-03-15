02fce5d99c9987d2d2e06ba621d72d92349503bd
nataliavrs <verasnatali4@gmail.com>
2021-03-15 11:51:46 +0100

    Menu font-size

--- main/public/js/app.js
+++ main/public/js/app.js
@@ -49917,8 +49917,18 @@ document.addEventListener('DOMContentLoaded', function () {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
+<<<<<<< Updated upstream
 __webpack_require__(/*! C:\proj21-team3\main\resources\js\app.js */"./resources/js/app.js");
 module.exports = __webpack_require__(/*! C:\proj21-team3\main\resources\sass\app.scss */"./resources/sass/app.scss");
+=======
+<<<<<<< Updated upstream
+__webpack_require__(/*! /home/angelo/Corso/Progetto_finale/proj21-team3/main/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/angelo/Corso/Progetto_finale/proj21-team3/main/resources/sass/app.scss */"./resources/sass/app.scss");
+=======
+__webpack_require__(/*! /Users/natalia/Desktop/Repos Laravel/proj21-team3/main/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/natalia/Desktop/Repos Laravel/proj21-team3/main/resources/sass/app.scss */"./resources/sass/app.scss");
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
 
 
 /***/ })
