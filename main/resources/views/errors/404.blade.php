44675f138969b2df727ad30b7efa89c54dbd6cfb
nataliavrs <verasnatali4@gmail.com>
2021-03-16 20:01:38 +0100

    404 fix

--- main/resources/views/errors/404.blade.php
+++ main/resources/views/errors/404.blade.php
@@ -1,19 +1,21 @@
 <!DOCTYPE HTML>
 <html>
 	<head>
-		<title>ERROR 404</title>
+		<title>Page not found | FooDuro </title>
 		<meta name="keywords" content="404 iphone web template, Android web template, Smartphone web template, free webdesigns for Nokia, Samsung, LG, Sony Ericsson, Motorola web design" />
 		<link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css"  media="all" />
+		 {{-- FAVICON --}}
+		 <link rel="icon" type="image/png" href="{{asset('storage/assets/favicon.png')}}"/>		
 	</head>
 	<body style="overflow:hidden;background-color:#2F294D;">
 		<div id="container_404" class="container-fluid" style="background-image: url('{{asset('storage/assets/404.svg')}}') ">
-			<div id="notfound">
-				<h1>404</h1>
-				<h1>PAGE NOT FOUND</h1>
+			<div id="notfound" >
+				<h1 class="quattro text-center font-weight-bolder">404</h1>
+				<h1 class="text-center font-weight-bolder">Ops, page not found!</h1>
 			</div>
 			<div id="error_404_button">
-				<a href="{{route('home')}}">
-					PLEASE TAKE ME HOME
+				<a href="{{route('index')}}">
+					<h1 class="font-weight-bolder">Take me home!</h1>					
 				</a>
 			</div>	
 		</div>
